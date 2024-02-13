d4d9afcc2d0fe9aec501d101ccc1041d81f9c208
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-02-13 15:48:21 +0530

    using matplotlib we plotted an histogram of bad psychological health

--- code/main.py
+++ code/main.py
@@ -3,8 +3,22 @@ from db import conn
 import matplotlib.pyplot as plt
 import seaborn as sns
 cursor = conn.cursor()
+import matplotlib.pyplot as plt
 df= pd.read_csv('Case Study\DATASET FOR CASESTUDY.csv')
-print(df.describe())
+# print(df.describe())
 
+cursor.execute('SELECT * FROM bad_psychological_health_frequency')
+data = cursor.fetchall()
+states = [item[0] for item in data]
+counts = [item[1] for item in data]
+plt.figure(figsize=(10, 6))
+plt.bar(states, counts, color='skyblue')
+plt.xlabel('States')
+plt.ylabel('Frequency of Bad Psychological Health')
+plt.title('Frequency of Bad Psychological Health State-wise')
+plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
+plt.tight_layout()  # Adjust layout to prevent clipping of labels
+plt.show()
+# print(data)
 cursor.close()
 conn.close()
