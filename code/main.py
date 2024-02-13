6503d1609fedf08246bb05f50dfe09e443375255
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-02-13 18:11:50 +0530

    created view for frequency of each year group by states

--- code/main.py
+++ code/main.py
@@ -6,16 +6,16 @@ cursor = conn.cursor()
 import matplotlib.pyplot as plt
 df= pd.read_csv('Case Study\DATASET FOR CASESTUDY.csv')
 # print(df.describe())
-
-cursor.execute('SELECT * FROM bad_psychological_health_frequency')
+year=2019
+cursor.execute('SELECT * FROM bad_psychological_health_frequency2017')
 data = cursor.fetchall()
 states = [item[0] for item in data]
 counts = [item[1] for item in data]
 plt.figure(figsize=(10, 6))
 plt.bar(states, counts, color='skyblue')
 plt.xlabel('States')
-plt.ylabel('Frequency of Bad Psychological Health')
-plt.title('Frequency of Bad Psychological Health State-wise')
+plt.ylabel(f'Frequency of Bad Psychological Health {year}')
+plt.title(f'Frequency of Bad Psychological Health State-wise {year}')
 plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
 plt.tight_layout()  # Adjust layout to prevent clipping of labels
 plt.show()
